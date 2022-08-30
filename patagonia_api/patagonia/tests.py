d1ab9630c47c5127be6fe7bdf23261bc44f1d554
brianjtarte <brianjtarte@gmail.com>
2022-08-29 19:01:05 -0700

    finished lab31

--- patagonia_api/patagonia/tests.py
+++ patagonia_api/patagonia/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Campsite
+
+
+class CampsiteTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        test_user = get_user_model().objects.create_user(username='test_user', password='pass')
+        test_user.save()
+
+        test_camp = Campsite.objects.create(author=test_user, title="Campsite1", body="A campsite")
+        test_camp.save()
+
+    def test_campsites_model(self):
+        campsite = Campsite.objects.get(id=1)
+        actual_author = str(campsite.author)
+        actual_title = str(campsite.title)
+        actual_body = str(campsite.body)
+        self.assertEqual(actual_author, "test_user")
+        self.assertEqual(actual_title, "Campsite1")
+        self.assertEqual(actual_body, "A campsite")
